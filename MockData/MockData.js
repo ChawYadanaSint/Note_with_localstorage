b62ce546eab5e63898cdab1ef885ed9289663650
ChawYadanaSint <ms.chawyadanasint@gmail.com>
2022-01-29 11:30:50 +0630

    First Commit

---
+++ MockData/MockData.js
@@ -0,0 +1,160 @@
+export var user = [
+  {
+    id: 1,
+    name: 'Chaw'
+  },
+  {
+    id: 2,
+    name: 'Aung Kaung Myat'
+  },
+  {
+    id: 3,
+    name: 'Nay Zin'
+  },
+  {
+    id: 4,
+    name: 'Hsu Myint Myat Kyaw'
+  }
+]
+
+
+export var user_ans = [
+  //Chaw Chaw
+  {
+    id: 1,
+    name: 'Chaw',
+    q_no: 'Q1',
+    answer: 'Beach'
+  },
+  {
+    id: 2,
+    name: 'Chaw',
+    q_no: 'Q2',
+    answer: 'Ghost'
+  },
+  {
+    id: 3,
+    name: 'Chaw',
+    q_no: 'Q3',
+    answer: 'Jelly'
+  },
+  {
+    id: 4,
+    name: 'Chaw',
+    q_no: 'Q4',
+    answer: 'Horror movies'
+  },
+
+  //Aung Kaung Myat
+  {
+    id: 5,
+    name: 'Aung Kaung Myat',
+    q_no: 'Q1',
+    answer: 'Beach'
+  },
+  {
+    id: 6,
+    name: 'Aung Kaung Myat',
+    q_no: 'Q2',
+    answer: 'Ghost'
+  },
+  {
+    id: 7,
+    name: 'Aung Kaung Myat',
+    q_no: 'Q3',
+    answer: 'Cookie'
+  },
+  {
+    id: 8,
+    name: 'Aung Kaung Myat',
+    q_no: 'Q4',
+    answer: 'Romance movies'
+  },
+
+  //Nay Zin
+  {
+    id: 9,
+    name: 'Nay Zin',
+    q_no: 'Q1',
+    answer: 'Mountain'
+  },
+  {
+    id: 10,
+    name: 'Nay Zin',
+    q_no: 'Q2',
+    answer: 'Water'
+  },
+  {
+    id: 11,
+    name: 'Nay Zin',
+    q_no: 'Q3',
+    answer: 'Cookie'
+  },
+  {
+    id: 12,
+    name: 'Nay Zin',
+    q_no: 'Q4',
+    answer: 'Horror movies'
+  },
+
+  //Hsu Myint Myat Kyaw
+  {
+    id: 13,
+    name: 'Hsu Myint Myat Kyaw',
+    q_no: 'Q1',
+    answer: "Beach"
+  },
+  {
+    id: 14,
+    name: 'Hsu Myint Myat Kyaw',
+    q_no: 'Q2',
+    answer: 'Water'
+  },
+  {
+    id: 15,
+    name: 'Hsu Myint Myat Kyaw',
+    q_no: 'Q3',
+    answer: 'Cookie'
+  },
+  {
+    id: 16,
+    name: 'Hsu Myint Myat Kyaw',
+    q_no: 'Q4',
+    answer: 'Romance movies'
+  },
+]
+
+export var questions = [
+  {
+    q_no: 'Q1',
+    question: 'Where does he/she want to go for vacation?',
+    answers: [
+      'Beach',
+      'Mountain',
+    ],
+  },
+  {
+    q_no: 'Q2',
+    question: 'What is he/she most afraid of?',
+    answers: [
+      'Water',
+      'Ghost',
+    ],
+  },
+  {
+    q_no: 'Q3',
+    question: 'What is his/her favorite snack?',
+    answers: [
+      'Cookie',
+      'Jelly',
+    ],
+  },
+  {
+    q_no: 'Q4',
+    question: 'Which does he/she like the most?',
+    answers: [
+      'Horror movies',
+      'Romance movies',
+    ],
+  }
+]
